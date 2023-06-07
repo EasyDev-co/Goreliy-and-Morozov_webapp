c3a2356b17d1ab4909a8e390f0b018bc5a5eb02a
Александр <tarasov.slavas2002@gmail.com>
2023-06-08 00:28:17 +0300

    Добавил пагинацию

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -95,6 +95,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
 }
 
 # Internationalization
